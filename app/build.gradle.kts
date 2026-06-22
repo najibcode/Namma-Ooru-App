877d8a8147934fcee3e41d686907ad7c1fcc1cf3
Najib <najibcode@gmail.com>
2026-06-22 14:53:10 +0530

    feat: enable Firebase AI dependency and set Java build configuration to automatic

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,7 +94,7 @@ dependencies {
   implementation(libs.androidx.room.runtime)
   implementation(libs.coil.compose)
   implementation(libs.converter.moshi)
-  // implementation(libs.firebase.ai)
+  implementation(libs.firebase.ai)
   implementation(libs.kotlinx.coroutines.android)
   implementation(libs.kotlinx.coroutines.core)
   implementation(libs.logging.interceptor)
